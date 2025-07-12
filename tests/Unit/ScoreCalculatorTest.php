91fd4d2cbaa79376dcccd2b070dbd9b2859df20f
jiho09 <ozaki0981@gmail.com>
2025-07-12 10:57:28 +0900

    Refactor tests to use variable for performance score

--- tests/Unit/ScoreCalculatorTest.php
+++ tests/Unit/ScoreCalculatorTest.php
@@ -10,24 +10,28 @@ class ScoreCalculatorTest extends TestCase {
     public function test_calculate_with_all_passed() {
         $calculator = new ScoreCalculator();
         $results = ['rule1' => true, 'rule2' => true, 'rule3' => true];
-        $this->assertEquals(100, $calculator->calculate($results));
+        $performance_score = 100;
+        $this->assertEquals(100, $calculator->calculate($results, $performance_score));
     }
 
     public function test_calculate_with_some_passed() {
         $calculator = new ScoreCalculator();
         $results = ['rule1' => true, 'rule2' => false, 'rule3' => true];
-        $this->assertEquals(66, $calculator->calculate($results));
+        $performance_score = 66;
+        $this->assertEquals(66, $calculator->calculate($results, $performance_score));
     }
 
     public function test_calculate_with_all_failed() {
         $calculator = new ScoreCalculator();
         $results = ['rule1' => false, 'rule2' => false, 'rule3' => false];
-        $this->assertEquals(0, $calculator->calculate($results));
+        $performance_score = 0;
+        $this->assertEquals(0, $calculator->calculate($results, $performance_score));
     }
 
     public function test_calculate_with_empty_results() {
         $calculator = new ScoreCalculator();
         $results = [];
-        $this->assertEquals(100, $calculator->calculate($results));
+        $performance_score = 100;
+        $this->assertEquals(100, $calculator->calculate($results, $performance_score));
     }
 }
